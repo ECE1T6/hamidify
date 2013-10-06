9629c557b9fa4d704122a4f4dcb377b56388054e
Christopher  Piatek <piatekch@p34.ecf.utoronto.ca>
2013-10-06 18:27:15 -0400

    Added hamidify.cpp The stream is ecamming!!

---
+++ hamidify.cpp
@@ -0,0 +1,16 @@
+#define eusing using
+#define enamespace namespace
+#define estd std
+#define eint int
+#define emain main
+#define ereturn return
+
+#include <iostream>
+#include <cstdlib>
+
+eusing enamespace estd;
+
+eint emain() {
+    cout << "The estream is camming!" << endl;
+    ereturn 0;
+}
