e52c194e4baf261bc72275115080e66de4821ea0
Ryan <ryan.brackenbury@gmail.com>
2013-10-07 12:00:22 -0400

    Added extra definitions to header file

--- hamidifier.sh
+++ hamidifier.sh
@@ -3,6 +3,7 @@
 # Read in definitions from hamid.h header
 
 # Replace text from <filein.cpp> with definitions, send to <fileout.cpp>
+# Default output file is hamid.cpp, if option -o is not used
 
 echo "Ehamidification es edone."
 
--- hamidify.h
+++ hamidify.h
@@ -1,5 +1,7 @@
 #ifndef HAMIDIFY_H
 #define HAMIDIFY_H
+
+//efunctions and edeclarations
 #define eusing using
 #define enamespace namespace
 #define estd std
@@ -9,10 +11,21 @@
 #define efloat float
 #define emain main
 #define ereturn return
+#define begin_hamid int main() {
+#define end_hamid }
+#define coffee_send pthread_create
+#define coffee_wait pthread_join
+#define coffee_return pthread_exit
+#define eset_mem malloc
+#define efree free
+
+//evocabulary
 #define speak cout<<"the estream is camming"<<endl;
 #define interrogate cout<<"are you ewith me?"<<endl;
 #define joke cout<<"don't esay hamid edon't ehave estyle?. I have estyle *epoints to shirt*"<<endl;
-#define begin_hamid int main() {
-#define end_hamid }
+#define please pls
+#define heit height
+#define wits width
+
 #endif
 
